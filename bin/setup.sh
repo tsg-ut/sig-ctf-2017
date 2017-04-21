2b22ebcc104c14aaa082139483cf68c4c5ce28e0
moratorium08 <moratorium0hiro8sanctuary@gmail.com>
2017-04-21 12:52:40 +0900

    Modify Typo

--- bin/setup.sh
+++ bin/setup.sh
@@ -8,27 +8,18 @@ BASE_ADDR="/home/vagrant/ctf-tools/"
 PREFIX="/install"
 
 lines=(
-"angr"
-"bindead"
 "checksec"
-"df"
 "gdb-heap"
 "gdb"
-"hash-identifier"
-"hashpump-partialhash"
-"hashpump"
 "libheap"
 "peda"
 "pwndbg"
 "pwntools"
-"python-paddingoracle"
-"python-pin"
 "rp++"
 )
 
 sudo apt-get install build-essential g++
 sudo apt-get -y install python2.7 python-pip python-dev libssl-dev libffi-dev
-sudo apt-get -y install python3-pip
 sudo pip install virtualenv virtualenvwrapper
 export WORKON_HOME=$HOME/.virtualenvs
 export PROJECT_HOME=$HOME/Devel
@@ -36,7 +27,7 @@ source /usr/local/bin/virtualenvwrapper.sh
 
 export PATH=/home/vagrant/ctf-tools/bin/:$PATH
 echo "export PATH=\"/home/vagrant/ctf-tools/bin:\$PATH\"" >> ~/.bashrc
-echo "source ctf-tools-venv-activate" >> ~.bashrc
+echo "source ctf-tools-venv-activate" >> ~/.bashrc
 
 # IFS=$'\n' lines=($(cat tool_list))
 for line in ${lines[@]}; do
