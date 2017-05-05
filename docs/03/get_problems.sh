d0da41d84be16a3e3d6264451393df75910ba396
moratorium08 <moratorium0hiro8sanctuary@gmail.com>
2017-05-05 23:45:22 +0900

    Impement get problem2

--- docs/03/get_problems.sh
+++ docs/03/get_problems.sh
@@ -9,6 +9,11 @@ wget https://github.com/TokyoWesterns/twctf-2016-problems/raw/master/Twin%20Prim
 7z e twin-primes.7z -oproblem1
 mv twin-primes.7z problem1
 
-cd ../
+mkdir problem2
 
+wget https://github.com/ctfs/write-ups-2016/raw/master/su-ctf-2016/crypto/high-speed-rsa-keygen-150/RSA-Keygen.tar.gz
+
+tar xvzf RSA-Keygen.tar.gz -C problem2
+
+mv RSA-Keygen.tar.gz problem2/
 
