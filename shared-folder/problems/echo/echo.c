929a296ebb3b4601aa7ae4978f21419cf26974d6
moratorium08 <moratorium0hiro8sanctuary@gmail.com>
2017-05-21 14:27:53 +0900

    Add echo.c

---
+++ shared-folder/problems/echo/echo.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+void binsh() {
+    system("/bin/sh");
+}
+
+// Simple Echo Program
+int main(void) {
+    char dump[101];
+    while(1) {
+        fgets(dump, 100, stdin);
+        printf(dump); // vuln
+        fflush(stdout);
+    }
+
+    return 0;
+}
