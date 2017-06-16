ed4d5a24d0301f8d6e6a89be67f8d80b28895f2b
moratorium08 <moratorium0hiro8sanctuary@gmail.com>
2017-06-16 19:00:08 +0900

    Change pwnlib to socket

--- docs/06/poa.py
+++ docs/06/poa.py
@@ -2,21 +2,32 @@
 from __future__ import division, print_function
 from Crypto.Cipher import AES
 import base64 as b64
+import socket
 #from server import decrypt_ok
-from pwn import remote
 
 host = "127.0.0.1"
 port = 3000
-r = remote(host, port)
+r = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+r.connect((host, port))
+
+def read_line(s):
+    ret = ''
+    while True:
+        c = s.recv(1)
+        if c == '\n' or c == '':
+            break
+        else:
+            ret += c
+    return ret
+
 
 def decrypt_ok(s):
-    r.sendline(b64.b64encode(s))
-    ret = r.recvline()
+    r.send(b64.b64encode(s) + "\n")
+    ret = read_line(r)
     print(ret)
     return ret[:4] == "True"
 
 s = "gINabWwYFPehHNZv56TWeGeg/Pa6dxDdKjN1Bq0+TYfnAFxd08c0AmVWGalPzFTFf7b27jJx2kxpSKdQuBbGd4r2GuLalTVM/S3RSGr0aOo="
-#s = "99hk11WWteOZsGBI5UOO5ULIXRaFNzpJs3MF9+EIzIu+7p2RlxY6QR1lFH8jFOcP"
 
 encrypted = b64.b64decode(s)
 
