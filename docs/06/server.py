7b12dc48cbb73eed1f0b3493b1f526034c8e711b
moratorium08 <moratorium0hiro8sanctuary@gmail.com>
2017-06-16 17:33:22 +0900

    Add server

---
+++ docs/06/server.py
@@ -0,0 +1,37 @@
+# coding:utf-8
+from __future__ import print_function
+from Crypto.Cipher import AES
+import base64 as b64
+
+
+
+with open("key", "r") as f:
+    key = f.readline()[:16]
+    iv_true = f.readline()[:16]
+
+with open("flag", "r") as f:
+    raw = f.read()
+def decrypt_ok(c):
+    cipher = AES.new(key, AES.MODE_CBC, IV=iv_true)
+    m = cipher.decrypt(c)
+    pad = ord(m[-1])
+    if pad > 16 or pad == 0:
+        return False
+    for i in range(pad):
+        #print(ord(m[-i-1]), end=" ")
+        if ord(m[-i-1]) != pad:
+            return False
+    return True
+
+if __name__ == '__main__':
+    print(key, iv_true, raw)
+
+    pad = 16 - (len(raw) % 16)
+    raw = raw + ''.join([chr(pad) for i in range(pad)])
+
+    cipher = AES.new(key, AES.MODE_CBC, IV=iv_true)
+    encrypted = cipher.encrypt(raw)
+    print(b64.b64encode(encrypted))
+    assert decrypt_ok(encrypted)
+
+
